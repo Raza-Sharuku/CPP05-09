957d3c7e765ef0ef9cf0f80271d6892da3428dd4
Shahrukh Raza <shahrukh4062@gmail.com>
2024-05-15 10:33:07 +0900

    done CPP06 leaks ok

--- CPP06/ex02/main.cpp
+++ CPP06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: razasharuku <razasharuku@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 11:24:39 by razasharuku       #+#    #+#             */
-/*   Updated: 2024/05/15 10:07:41 by razasharuku      ###   ########.fr       */
+/*   Updated: 2024/05/15 10:32:30 by razasharuku      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int main(void)
 	Base *p;
 
 	srand((unsigned)time(NULL));
-	std::cout << "\033[1;33m" << "++++++++++++++++++ test start 1 ++++++++++++++++++++\n" << "\033[0m" << std::endl;
+	std::cout << "\033[1;33m" << "++++++++++++++++++ アップキャスト test start 1 ++++++++++++++++++++\n" << "\033[0m" << std::endl;
 
 	p = generate();
 	if (p == NULL)
@@ -32,25 +32,57 @@ int main(void)
     std::cout << "\n-----------------------\n"  << std::endl;
 	identify(*p);
 
-    std::cout << "\033[1;33m" << "\n++++++++++++++++++ test end +++++++++++++++++++++++\n" << "\033[0m" << std::endl;
-
 	delete p;
+    std::cout << "\033[1;33m" << "\n++++++++++++++++++++++++   test end  +++++++++++++++++++++++++++++\n" << "\033[0m" << std::endl;
 
-	std::cout << "\033[1;33m" << "++++++++++++++++++ test start 2 +++++++++++++++++++\n" << "\033[0m" << std::endl;
-	Base *q = new Base;
-	
-    std::cout << "Test of Base class(which none of A,B,C) !!\n"  << std::endl;
+
+
+
+	std::cout << "\033[1;36m" << "++++++++++++++++++ ダウンキャスト test start 2 ++++++++++++++++++++\n" << "\033[0m" << std::endl;
+	A *a = new A;
+	Base *down = a;
+	// Base *down = new Base;
+	A *q = dynamic_cast<A*>(down);
+
+	if (q == NULL)
+	{
+    	std::cout << "Down cast failed!!"  << std::endl;
+		return (0);
+	}
+    std::cout << "q is successfully down_cast !!\n"  << std::endl;
+    
     std::cout << "-----------------------\n"  << std::endl;
 	identify(q);
     std::cout << "\n-----------------------\n"  << std::endl;
 	identify(*q);
-	
+
 	delete q;
+	// delete down;
+    std::cout << "\033[1;36m" << "\n++++++++++++++++++++++++   test end  +++++++++++++++++++++++++++++\n" << "\033[0m" << std::endl;
+
+
+
+
+
+	std::cout << "\033[1;33m" << "++++++++++++++++++ test start 3 +++++++++++++++++++\n" << "\033[0m" << std::endl;
+	Base *r = new Base;
+	
+    std::cout << "Test of Base class(which none of A,B,C) !!\n"  << std::endl;
+    std::cout << "-----------------------\n"  << std::endl;
+	identify(r);
+    std::cout << "\n-----------------------\n"  << std::endl;
+	identify(*r);
+	
+	delete r;
     std::cout << "\033[1;33m" << "\n++++++++++++++++++ test end ++++++++++++++++++++++\n" << "\033[0m" << std::endl;
+
 	return (0);
 }
 
+
+
 __attribute__((destructor))
-static void destructor() {
+static void destructor() 
+{
     system("leaks -q ex");
 }
