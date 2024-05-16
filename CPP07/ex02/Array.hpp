40aef21af4e294939b1bb75a6b77981a5090fb4a
Shahrukh Raza <shahrukh4062@gmail.com>
2024-05-16 10:09:06 +0900

    ex02 done

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: razasharuku <razasharuku@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 13:36:02 by razasharuku       #+#    #+#             */
-/*   Updated: 2024/05/15 17:43:30 by razasharuku      ###   ########.fr       */
+/*   Updated: 2024/05/16 09:46:35 by razasharuku      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,14 +35,76 @@ class Array
         Array &operator=(const Array &other);
         // デストラクタ
         ~Array(void);
-        // []演算子
-        T &operator[](unsigned int index);
+        // []添字演算子
+        T &operator[](unsigned int i);
 
         // メンバ関数
         unsigned int size() const;
 
 
 };
+
+template <typename T>
+Array<T>::Array(void) : elements(NULL), length(0)
+{    
+}
+
+template <typename T>
+Array<T>::Array(unsigned int n) : elements(new T[n]), length(n)
+{    
+}
+
+template <typename T>
+Array<T>::Array(const Array &other) : elements(new T[other.length]), length(other.length)
+{
+    unsigned int i = 0;
+    while (i < length)
+    {
+        this->elements[i] = other.elements[i];
+        i++;
+    }
+    return ;
+}
+
+template <typename T>
+Array<T> &Array<T>::operator=(const Array& other)
+{
+    if (this != &other)
+    {
+        delete[] elements;
+        this->length = other.length;
+        elements = new T[length];
+        unsigned int i = 0;
+        while (i < length)
+        {
+            this->elements[i] = other.elements[i];
+            i++;
+        }
+    }
+    return (*this);
+}
+
+template <typename T>
+Array<T>::~Array(void)
+{
+    delete[] elements;
+}
+
+template <typename T>
+T &Array<T>::operator[](unsigned int i)
+{
+    if (i >= this->length)
+    {
+        throw std::exception();
+    }
+    return this->elements[i];
+}
+
+template <typename T>
+unsigned int Array<T>::size() const
+{
+    return (length);
+}
 
 
 #endif
--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -6,71 +6,84 @@
 /*   By: razasharuku <razasharuku@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 11:24:39 by razasharuku       #+#    #+#             */
-/*   Updated: 2024/05/15 17:52:51 by razasharuku      ###   ########.fr       */
+/*   Updated: 2024/05/16 10:08:48 by razasharuku      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
 
 
-template <typename T>
-Array<T>::Array(void) : elements(NULL), length(0)
-{    
-}
-
-template <typename T>
-Array<T>::Array(unsigned int n) : elements(new T[n]), length(n)
-{    
-}
-
-template <typename T>
-Array<T>::Array(const Array &other) : elements(new T[other.length]), length(other.length)
-{
-    unsigned int i = 0;
-    while (i < length)
-    {
-        this->elements[i] = other.elements[i];
-        i++;
-    }
-    return ;
-}
-
-template <typename T>
-Array<T> &Array<T>::operator=(const Array& other)
-{
-    if (this != &other)
-    {
-        delete [] elements;
-        this->length = other.length;
-        elements = new T[length];
-        unsigned int i = 0;
-        while (i < length)
-        {
-            this->elements[i] = other.elements[i];
-            i++;
-        }
-    }
-    return (*this);
-}
-
-template <typename T>
-Array<T>::~Array(void)
-{
-    delete [] this->elements;
-}
-
-
-
 int main() 
 {
-	std::cout << "\033[1;33m" << "++++++++++++++++++ test start 1 ++++++++++++++++\n" << "\033[0m" << std::endl;
+	std::cout << "\033[31m" << "++++++++++++++++++ test start 1 ++++++++++++++++\n" << "\033[0m" << std::endl;
+
+    Array<double> arr1;
+    std::cout << "arr1 size: " << arr1.size() << std::endl;
+	std::cout << "\033[31m" << "\n++++++++++++++++++ test finish 1 +++++++++++++++" << "\033[0m" << std::endl;
 
 
-	std::cout << "\033[1;33m" << "\n++++++++++++++++++ test finish 1 +++++++++++++++" << "\033[0m" << std::endl;
-	std::cout << "\033[1;36m" << "++++++++++++++++++ test start 2 ++++++++++++++++\n" << "\033[0m" << std::endl;
+	std::cout << "\033[32m" << "++++++++++++++++++ test start 2 ++++++++++++++++\n" << "\033[0m" << std::endl;
+    Array<int> arr2(5);
+    std::cout << "arr2 size: " << arr2.size() << std::endl;
+    for (unsigned int i = 0; i < arr2.size(); ++i) 
+    {
+        arr2[i] = i;
+    }
+    for (unsigned int i = 0; i < arr2.size(); ++i) 
+    {
+        std::cout << "arr2[" << i << "]: " << arr2[i] << std::endl;
+    }
+	std::cout << "\033[32m" << "\n++++++++++++++++++ test finish 2 +++++++++++++++\n" << "\033[0m" << std::endl;
 
 
-	std::cout << "\033[1;36m" << "\n++++++++++++++++++ test finish 2 +++++++++++++++\n" << "\033[0m" << std::endl;
+	std::cout << "\033[33m" << "++++++++++++++++++ test start 3 ++++++++++++++++\n" << "\033[0m" << std::endl;
+    Array<int> arr3(arr2);
+    std::cout << "arr3 size: " << arr3.size() << std::endl;
+    for (unsigned int i = 0; i < arr3.size(); ++i) 
+    {
+        std::cout << "arr3[" << i << "]: " << arr3[i] << std::endl;
+    }
+	std::cout << "\033[33m" << "\n++++++++++++++++++ test finish 3 +++++++++++++++\n" << "\033[0m" << std::endl;
+
+
+	std::cout << "\033[34m" << "++++++++++++++++++ test start 4 ++++++++++++++++\n" << "\033[0m" << std::endl;
+    Array<int> arr4;
+    arr4 = arr2;
+    std::cout << "arr4 size: " << arr4.size() << std::endl;
+    for (unsigned int i = 0; i < arr4.size(); ++i) 
+    {
+        std::cout << "arr4[" << i << "]: " << arr4[i] << std::endl;
+    }
+	std::cout << "\033[34m" << "\n++++++++++++++++++ test finish 4 +++++++++++++++\n" << "\033[0m" << std::endl;
+
+
+	std::cout << "\033[35m" << "++++++++++++++++++ test start 5 ++++++++++++++++\n" << "\033[0m" << std::endl;
+    Array<double> arr5(10);
+    std::cout << "arr5 size: " << arr5.size() << std::endl;
+    double d = 0.05;
+    for (unsigned int i = 0; i < arr5.size(); ++i) 
+    {
+        arr5[i] = d;
+        d += 0.5;
+    }
+    for (unsigned int i = 0; i < arr5.size(); ++i) 
+    {
+        std::cout << "arr5[" << i << "]: " << arr5[i] << std::endl;
+    }
+	std::cout << "\033[35m" << "\n++++++++++++++++++ test finish 5 +++++++++++++++\n" << "\033[0m" << std::endl;
+
+
+	std::cout << "\033[36m" << "++++++++++++++++++ test start 6 ++++++++++++++++\n" << "\033[0m" << std::endl;
+    try 
+    {
+        arr2[10] = 10;
+    }
+    catch (const std::exception& e)
+    {
+        std::cout << "Out of bounds access attempted." << std::endl;
+    }
+	std::cout << "\033[35m" << "\n++++++++++++++++++ test finish 5 +++++++++++++++\n" << "\033[0m" << std::endl;
+
 
     return 0;
 }
